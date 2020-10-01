48f44772c95255fea8b424b9c046425ee92b7d00
lolsmartPR <72013375+lolsmartPR@users.noreply.github.com>
2020-10-01 15:45:30 +0530

    made changes and solved the error

--- 12.3 string_formatting.py.py
+++ 12.3 string_formatting.py.py
@@ -1,10 +1,10 @@
 age = 24
-print("My age is " + str(age) + " years")
+print("My age is done " + str(age) + " years")
 
 age = 24
 print("My age is {0} years".format(age))
 
-print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7} ".format(31, "January", "March", "May", "July", "August", "October", "December"))
+print("There are {1} days in {1}, {2}, {3}, {4}, {5}, {6} and {7} ".format(31, "January", "March", "May", "July", "August", "October", "December"))
 
 print("January: {2}, February: {0}, March: {2}, April: {1}, May: {2}, June: {1}, July: {2}, August: {2}, September: {1}, October: {2}, November: {1}, December: {2}".format(28, 30, 31))
 
@@ -44,4 +44,4 @@ print("Pi is approximately {0:62.50f}".format(22 / 7))
 print("Pi is approximately {0:72.50f}".format(22 / 7))
 
 for i in range(1, 12):
-    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+    print("No. { } squared is { } and cubed is {:4}".format(i, i ** 2, i ** 3))
